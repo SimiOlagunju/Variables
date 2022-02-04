d3f543944aa32a01814363e8502b5e4cd7988b02
Simi Olag <simiolagunju@gmail.com>
2022-02-04 14:29:22 +0100

    Variable

---
+++ variables.py
@@ -0,0 +1,40 @@
+# Variables are containers used to store data values
+
+# strings: They're alphabets, characters or words
+name = 'Ola'
+print(name)
+
+# integers: They're in whole numbers
+Age = 30
+
+print(Age)
+
+# Float: They're decimal whole numbers
+tax = 45.20
+
+print(tax)
+
+# Boolean: They return the True or False value
+can_swim = True
+can_swim = False
+
+print(can_swim)
+
+#*****************************************************************************
+# Here is a small program that stores data of a student in an institution:
+
+#*****************************************************************************
+
+first_name = 'Busayo'
+last_name = 'Fadola'
+age = 28
+course = 'Cybersecurity'
+height = 5.8
+is_a_firstclass_student = 'True'
+
+print(first_name +' ' +  last_name + ' ' + str(age) + ' ' + course + ' ' + str(height) + ' ' + is_a_firstclass_student)
+
+
+
+
+
